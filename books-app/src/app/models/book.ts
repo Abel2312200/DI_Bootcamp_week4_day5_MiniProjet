fc9733179d41662ae9f17bc870be2bc31e917734
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-01-16 14:07:58 +0000

    relevant datas retrieved

--- books-app/src/app/models/book.ts
+++ books-app/src/app/models/book.ts
@@ -1,4 +1,3 @@
 export class Book {
-    
     constructor(title: string,authors: string[],coverImage: string){}
 }
---
+++ books-app/src/app/models/testdata.ts
@@ -0,0 +1,7 @@
+const dataTest = {
+        title: "The Coma",
+        authors: [
+          "Alex Garland"
+        ],
+        thumbnail : "http://books.google.com/books/content?id=LB2oAAAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    } 
